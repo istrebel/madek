cf27e357f60832361fcf6469786b689a53685db2
Thomas Schank <DrTom@schank.ch>
2012-08-21 12:02:52 +0200

    added 004 groups_users

---
+++ db/migrate/004_create_groups_users.rb
@@ -0,0 +1,25 @@
+# -*- encoding : utf-8 -*-
+class CreateGroupsUsers < ActiveRecord::Migration
+
+  def up
+
+    create_table  :groups_users, id: false do |t|
+
+      t.integer :group_id
+      t.integer :user_id
+
+    end
+
+    add_index :groups_users, [:user_id,:group_id], unique: true
+    add_index :groups_users, [:group_id,:user_id]
+
+    add_foreign_key :groups_users, :users, dependent: :delete
+    add_foreign_key :groups_users, :groups, dependent: :delete
+
+  end
+
+  def down
+    drop_table :groups_users
+  end
+
+end
