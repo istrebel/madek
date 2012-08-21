3bb82b421fdf18583e0c6f377643935b836ade47
Thomas Schank <DrTom@schank.ch>
2012-08-21 14:18:42 +0200

    added previews

---
+++ db/migrate/008_create_previews.rb
@@ -0,0 +1,25 @@
+class CreatePreviews < ActiveRecord::Migration
+
+  def up
+    create_table :previews, :force => true do |t|
+      t.belongs_to :media_file
+
+      t.integer    :height
+      t.integer    :width
+      t.string     :content_type
+      t.string     :filename
+      t.string     :thumbnail
+
+      t.timestamps
+    end
+
+    add_index :previews, :media_file_id
+
+    add_foreign_key :previews, :media_files , dependent: :delete
+  end
+
+  def down
+    drop_table :previews
+  end
+
+end
